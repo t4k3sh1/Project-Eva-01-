cbcef09524dbf5c6882de39ab4438a8f9a3918b4
Mercurio <47455213+NotLugozzi@users.noreply.github.com>
2022-08-08 19:24:27 +0200

    Boring Backend stuff
    Modified backend in preparation of merge with AI, now serial port reads what it's supposed to from the connection textbox, added a wUnimplemented tag for all features we want to merge but are still working on; minor code refactoring

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import serial
 #porta sees 
 with serial.Serial() as ser:
     ser.baudrate = 19200
-    ser.port = '/dev/ttyUSB1'
+    
 
 
 class Test(Ui_Pannello):
@@ -12,8 +12,15 @@ class Test(Ui_Pannello):
         self.setupUi(window)
         self.pushButton_2.clicked.connect(self.Serialstart)
         self.pushButton.clicked.connect(self.Quit)
+        self.pushButton_6.clicked.connect(self.EmStop)
+        self.pushButton_3.clicked.connect(self.wUnimplemented)#self.slowWalk
+        self.pushButton_4.clicked.connect(self.wUnimplemented)#self.fastWalk
+        self.pushButton_5.clicked.connect(self.wUnimplemented)#self.boringAI
 
     def Serialstart(self):
+        serialUID = self.textEdit.toPlainText()
+        print(serialUID) #debug code :hahaball:
+        ser.port = serialUID
         ser.open()
         print(ser.name)
         hs = ser.readline()
@@ -21,6 +28,29 @@ class Test(Ui_Pannello):
 
     def Quit(self):
         sys.exit()
+    
+    def EmStop(self):
+        #ser.write(b'EMS\n')
+        print("Emergency stop called! Disabling motors and resetting to original pos")
+        #ser.write(b"0xn -a -r -nrh\n") # pos 0 on all motors, removes power (-a), restores relative positioning (-r), doesn't home after resetting (-nrh)
+        print("0xn called, you'll need to restore active connection using the connect button. it is highly advised to also flush connection stats before attempting sync")
+        #disables serial port
+        #ser.close()
+        #need reconnection to restore stream
+    def wUnimplemented(self):
+        print("Uh Oh! it looks like the feature you're trying to use is still being worked on by our team, please wait patiently and check for repository updates often!")
+
+
+    #def slowWalk(self):
+        #ser.write(b"\n")
+        #ser.read
+        #print("continuing walking loop, client reports no warning, check 3dUI for more information")
+    #def fastWalk(self):
+        #ser.write(b"\n")
+        #ser.read
+        #print("continuing walking loop, client reports no warning, check 3dUI for more information")
+    #def boringAI(self):
+        #future mercury will definitely love working on the ai backend :skull:
 
 app = QtWidgets.QApplication(sys.argv)
 Pannello = QtWidgets.QWidget()
